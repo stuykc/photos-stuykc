a050d04f41169b7c6c207728c77e50231e7eab32
Danny Qiu <dqiu55@gmail.com>
2013-08-24 01:41:47 -0400

    Add functionality for uploading photos to the blobstore

---
+++ photos/__init__.py
@@ -0,0 +1,5 @@
+import os, jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+    extensions=['jinja2.ext.autoescape'])
--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,5 @@
-import os, webapp2, jinja2
-
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
-    extensions=['jinja2.ext.autoescape'])
+from photos import JINJA_ENVIRONMENT
+import webapp2
 
 class MainPage(webapp2.RequestHandler):
 
@@ -10,24 +7,13 @@ class MainPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render({}))
 
-class UploadForm(webapp2.RequestHandler):
-
-    def get(self):
-        template = JINJA_ENVIRONMENT.get_template('upload.html')
-        self.response.write(template.render({}))
-
-    def post(self):
-        template = JINJA_ENVIRONMENT.get_template('upload.html')
-        self.response.write(template.render(template_values))
-
 class AdminPage(webapp2.RequestHandler):
     
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('admin.html')
         self.response.write(template.render(template_values))
 
-application = webapp2.WSGIApplication([
-    ('/upload', UploadForm),
+app = webapp2.WSGIApplication([
     ('/admin', AdminPage),
     ('/.*', MainPage)
 ], debug=True)
