43810b2a44fd431437be1f2d6489b05f0d415b10
Danny Qiu <dqiu55@gmail.com>
2013-09-01 01:59:54 -0400

    Make admin page more interactive using modals and start on approving/deleting photos

--- photos/admin.py
+++ photos/admin.py
@@ -33,14 +33,16 @@ def fetchPhotos():
             image_link_thumb = image_link + '=s150-c'
             image_link_full = image_link + '=s0'
         else:
-            image_link_thumb = image_link_full = '/admin/%s' % photo.blob_key.key()
+            image_link_thumb = image_link_full = '/admin/serve/%s' % photo.blob_key.key()
 
         image_fetch.append({'name': photo.name,
                             'id': photo.stuyId,
                             'email': photo.email,
                             'event': photo.event,
+                            'file': photo.blob_key.filename,
                             'thumbnail_link': image_link_thumb,
-                            'image_link': image_link_full
+                            'image_link': image_link_full,
+                            'key': photo.blob_key.key()
                            })
     return image_fetch
 
@@ -53,6 +55,8 @@ class BlobHandler(blobstore_handlers.BlobstoreDownloadHandler):
         self.send_blob(blob_info)
 
 app = webapp2.WSGIApplication([
-    ('/admin', AdminPage),
-    ('/admin/([^/]+)?', BlobHandler)
+    ('/admin/serve/([^/]+)?', BlobHandler),
+#    ('/admin/approve/([^/]+)?', ApproveHandler),
+#    ('/admin/delete/([^/]+)?', DeleteHandler),
+    ('/admin.*', AdminPage)
 ], debug=True)
